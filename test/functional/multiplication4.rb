cd3f2c6141658234e92eea714f0d385e901d5cbb
beer_takuya <beer_takuya@hotmail.com>
2012-06-04 10:05:45 +0900

    tddbook 4th chapter

---
+++ test/functional/multiplication4.rb
@@ -0,0 +1,39 @@
+# !/usr/bin/ruby
+#coding : utf-8
+require "test/unit"
+
+class Test_Dollar < Test::Unit::TestCase
+  
+  def test_multiplication
+    five = Dollar.new(5)
+    product = five.times(2)
+    assert_equal(10,product.get_amount)
+    product = five.times(3)
+    assert_equal(15,product.get_amount)
+  end
+  
+  def test_equality
+    assert(Dollar.new(5).equals(Dollar.new(5)))
+    !assert(Dollar.new(5).equals(Dollar.new(5)))
+  end
+  
+end
+
+class Dollar
+  def initialize(amount)
+    @amount = amount
+  end
+  
+  def times(multiplier)
+    return Dollar.new(@amount*multiplier)
+  end
+  
+  # @amountに直接アクセスできないのでgetterを定義
+  def get_amount()
+    return @amount
+  end
+  
+  def equals(dollar)
+    return @amount == dollar.get_amount
+  end
+end
